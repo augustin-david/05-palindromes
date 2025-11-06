ffd073b812084918396f0cd7d484945d90ec01f1
augustin-david <augustin.david@edu.esiee.fr>
2025-11-06 09:51:17 +0000

    Ajout du module pour tester les palindromes avec gestion des accents et de la casse

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+"""Module principal pour vérifier les palindromes."""
+import unicodedata
 #### Fonction secondaire
 
 
 def ispalindrome(p):
+    """
+    Vérifie si une chaîne de caractères est un palindrome.
+    """
+    p = unicodedata.normalize('NFD', p.lower())
+    lettres = "".join(c for c in p if c.isalnum() and unicodedata.category(c) != 'Mn')
+    return lettres == lettres[::-1]
 
-    # votre code ici
-    
-    return False
 
 #### Fonction principale
 
 
 def main():
-
+    """
+    Fonction principale pour tester ispalindrome.
+    """
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
